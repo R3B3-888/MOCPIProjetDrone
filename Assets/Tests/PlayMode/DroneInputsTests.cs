9bc04f916ec9311f7e6dca99e6d4087a0269904d
R3B3-888 <56821593+R3B3-888@users.noreply.github.com>
2022-02-15 18:41:19 +0100

    Update on the Drone Inputs Tests

--- Assets/Tests/PlayMode/DroneInputsTests.cs
+++ Assets/Tests/PlayMode/DroneInputsTests.cs
@@ -9,69 +9,96 @@ using UnityEditor;
 public class DroneInputsTests
 {
     private GameObject _dronePrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Drones/DroneControllable.prefab");
+
     [UnityTest]
     public IEnumerator _0_Drone_Prefab_OK()
     {
-        var droneController = _dronePrefab.GetComponent<IP_Drone_Controller>();
+        // var droneController = _dronePrefab.GetComponent<IP_Drone_Controller>();
         var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(0,50,0), Quaternion.identity);
         Assert.IsNotNull(prefabInstance);
         yield return null;
     }
 
     [UnityTest]
-    public IEnumerator _1_Input_Cyclic_X_Neg ()
+    public IEnumerator _1_Input_Cyclic_Roll_Left ()
     {
-        //:TODO:15/02/22:ALEXIS:Tests to implement
-        yield return new System.NotImplementedException();
+        var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(0, -100, 0), Quaternion.identity);
+        prefabInstance.GetComponent<IP_Drone_Inputs>().Cyclic = new Vector2(-1, 0);
+        yield return new WaitForSeconds(5f);
+        Assert.IsTrue(prefabInstance.transform.position.x < 0);
+        Assert.AreEqual(-100, prefabInstance.transform.position.y);
+        Assert.AreEqual(0, prefabInstance.transform.position.z);
     }
     
     [UnityTest]
-    public IEnumerator _2_Input_Cyclic_X_Pos ()
+    public IEnumerator _2_Input_Cyclic_Roll_Right ()
     {
-        //:TODO:15/02/22:ALEXIS:Tests to implement
-        yield return new System.NotImplementedException();
+        var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(0, -80, 0), Quaternion.identity);
+        prefabInstance.GetComponent<IP_Drone_Inputs>().Cyclic = new Vector2(1, 0);
+        yield return new WaitForSeconds(.1f);
+        Assert.IsTrue(prefabInstance.transform.position.x > 0);
+        Assert.AreEqual(-80, prefabInstance.transform.position.y);
+        Assert.AreEqual(0, prefabInstance.transform.position.z);
     }
     [UnityTest]
-    public IEnumerator _3_Input_Cyclic_Y_Neg ()
+    public IEnumerator _3_Input_Cyclic_Pitch_Forward ()
     {
-        //:TODO:15/02/22:ALEXIS:Tests to implement
-        yield return new System.NotImplementedException();
+        var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(0, -60, 0), Quaternion.identity);
+        prefabInstance.GetComponent<IP_Drone_Inputs>().Cyclic = new Vector2(0, 1);
+        yield return new WaitForSeconds(.1f);
+        Assert.IsTrue(prefabInstance.transform.position.z > 0);
+        Assert.AreEqual(0, prefabInstance.transform.position.x);
+        Assert.AreEqual(-60, prefabInstance.transform.position.y);
     }
     
     [UnityTest]
-    public IEnumerator _4_Input_Cyclic_Y_Pos ()
+    public IEnumerator _4_Input_Cyclic_Pitch_Backward ()
     {
-        //:TODO:15/02/22:ALEXIS:Tests to implement
-        yield return new System.NotImplementedException();
+        var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(0, -40, 0), Quaternion.identity);
+        prefabInstance.GetComponent<IP_Drone_Inputs>().Cyclic = new Vector2(0, -1);
+        yield return new WaitForSeconds(.1f);
+        Assert.IsTrue(prefabInstance.transform.position.z < 0);
+        Assert.AreEqual(0, prefabInstance.transform.position.x);
+        Assert.AreEqual(-40, prefabInstance.transform.position.y);
     }
     
     [UnityTest]
     public IEnumerator _5_Input_Pedals_Neg ()
     {
-        //:TODO:15/02/22:ALEXIS:Tests to implement
-        yield return new System.NotImplementedException();
+        var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        prefabInstance.GetComponent<IP_Drone_Inputs>().Pedals = -1f;
+        yield return null;
     }
 
     [UnityTest]
     public IEnumerator _6_Input_Pedals_Pos ()
     {
-        //:TODO:15/02/22:ALEXIS:Tests to implement
-        yield return new System.NotImplementedException();
+        var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        prefabInstance.GetComponent<IP_Drone_Inputs>().Pedals = 1f;
+        yield return null;
     }
 
     
     [UnityTest]
     public IEnumerator _7_Input_Throttle_Neg ()
-    {
-        //:TODO:15/02/22:ALEXIS:Tests to implement
-        yield return new System.NotImplementedException();
+    {        
+        var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(-10, 0, 0), Quaternion.identity);
+        prefabInstance.GetComponent<IP_Drone_Inputs>().Throttle = -1f;
+        yield return new WaitForSeconds(.1f);
+        Assert.IsTrue(prefabInstance.transform.position.y < 0);
+        Assert.AreEqual(-10, prefabInstance.transform.position.x);
+        Assert.AreEqual(0, prefabInstance.transform.position.z);
     }
 
     
     [UnityTest]
     public IEnumerator _8_Input_Throttle_Pos ()
     {
-        //:TODO:15/02/22:ALEXIS:Tests to implement
-        yield return new System.NotImplementedException();
+        var prefabInstance = Object.Instantiate(_dronePrefab, new Vector3(10, 0, 0), Quaternion.identity);
+        prefabInstance.GetComponent<IP_Drone_Inputs>().Throttle = 1f;
+        yield return new WaitForSeconds(.1f);
+        Assert.IsTrue(prefabInstance.transform.position.y > 0);
+        Assert.AreEqual(10, prefabInstance.transform.position.x);
+        Assert.AreEqual(0, prefabInstance.transform.position.z);
     }
 }
