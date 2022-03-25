288ce078cc641ffb3585713e902a393e9a63e7d6
R3B3-888 <56821593+R3B3-888@users.noreply.github.com>
2022-03-25 15:50:10 +0100

    Simplify the swarm state string recuperation

--- Assets/Scripts/Menus/DisplaySwarmState.cs
+++ Assets/Scripts/Menus/DisplaySwarmState.cs
@@ -9,51 +9,10 @@ namespace Menus
     {
         [SerializeField] private SwarmManager _swarm;
         [SerializeField] private Text _text;
-        [SerializeField] private GameObject _sideMenu;
-
-        private void Awake()
-        {
-            _text.text = "Spawning";
-        }
 
         private void Update()
         {
-            // if (_sideMenu.GetComponent<SideMenu>().isShown is false) return;
-            var stateInString = "";
-            switch (_swarm.state)
-            {
-                case GameState.SpawningDrones:
-                    stateInString = "SpawningDrones";
-                    break;
-                case GameState.Standby:
-                    stateInString = "Standby";
-                    break;
-                case GameState.TakeOff:
-                    stateInString = "TakeOff";
-                    break;
-                case GameState.OnTheWayIn:
-                    stateInString = "On The Way In";
-                    break;
-                case GameState.Monitoring:
-                    stateInString = "Monitoring";
-                    break;
-                case GameState.OnTheWayBack:
-                    stateInString = "On The Way Back";
-                    break;
-                case GameState.Landing:
-                    stateInString = "";
-                    break;
-                case GameState.Crashing:
-                    stateInString = "";
-                    break;
-                case GameState.Repositioning:
-                    stateInString = "Repositioning";
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-
-            _text.text = stateInString;
+            _text.text = $"{_swarm.state}";
         }
     }
 }
